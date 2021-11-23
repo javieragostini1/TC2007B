3c2e47a2a3a1193f720c8c42103f808bb7f8d8ea
Gilberto Ramos Salinas <58603364+Gilberto119@users.noreply.github.com>
2021-11-23 10:15:25 -0600

    New website update
    Javi por fa rifate el login con las credenciales

---
+++ PrepaNet Control/assets/js/login.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('#log');
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  
+  if (user) {
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+      // window.location.replace("./control.html")
+
+      window.location.replace("../index.html")
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+    
+    }
+
+  }
+  
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // window.alert("Bienvenido");
+  var userEmail = form.email.value;
+  var userPass = form.password.value;
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+    
+  });
+  
+  
+});
+
+// getting data
+db.collection('Users').get().then(snapshot => {
+  snapshot.docs.forEach(doc => {
+      validation(doc);
+  });
+});
+
+function logout(){
+  firebase.auth().signOut();
+}
